512a85f35292bc578c4e6ba9f84ca5b03132336b
meganma <meganma45@gmail.com>
2023-04-22 16:03:10 -0700

    Added API Key

---
+++ weather/Secret.swift
@@ -0,0 +1,13 @@
+//
+//  Secret.swift
+//  weather
+//
+//  Created by Megan Ma on 4/22/23.
+//  Copyright © 2023 David Rozmajzl. All rights reserved.
+//
+
+import Foundation
+
+struct Secret {
+    static let APPID = "57bc0046f4bd702ed7db3f6945311e4c"
+}
